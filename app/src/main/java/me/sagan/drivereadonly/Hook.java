a82e1569ff107ee3193da71b3ae87fbf6166df71
Jacques De SAGAN <jacquesde@sagan.me>
2019-02-02 23:16:48 +0800

    finish hook GoogleAuthUtil.getToken codes

--- app/src/main/java/me/sagan/drivereadonly/Hook.java
+++ app/src/main/java/me/sagan/drivereadonly/Hook.java
@@ -10,6 +10,7 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
 import android.accounts.Account;
 import android.content.Context;
+import android.os.Bundle;
 
 public class Hook implements IXposedHookLoadPackage {
   public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
@@ -28,7 +29,6 @@ public class Hook implements IXposedHookLoadPackage {
       @Override
       protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
         String scope = (String) param.args[0];
-        XposedBridge.log("DriveReadonly Find  scope of package " + lpparam.packageName + " init scope " + scope);
         if (scope.equals("https://www.googleapis.com/auth/drive")) {
           param.args[0] = "https://www.googleapis.com/auth/drive.readonly";
         }
@@ -39,21 +39,36 @@ public class Hook implements IXposedHookLoadPackage {
       @Override
       protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
         String scope = (String) param.args[1];
-        XposedBridge.log("DriveReadonly Find  scope of package " + lpparam.packageName + " init scope(int,String) " + scope);
         if (scope.equals("https://www.googleapis.com/auth/drive")) {
           param.args[1] = "https://www.googleapis.com/auth/drive.readonly";
         }
       }
     });
 
-    findAndHookMethod("com.google.android.gms.auth.GoogleAuthUtil", lpparam.classLoader, "getToken", Context.class, String.class, String.class, new XC_MethodHook() {
-      @Override
-      protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-        String oauthScopes = (String) param.args[2];
-        if (oauthScopes.equals("oauth2: https://www.googleapis.com/auth/drive")) {
-          param.args[2] = "oauth2: https://www.googleapis.com/auth/drive.readonly";
+    XC_MethodHook hook = new GoogleAuthUtilGetTokenHook();
+
+    findAndHookMethod("com.google.android.gms.auth.GoogleAuthUtil", lpparam.classLoader, "getToken", Context.class, Account.class, String.class, Bundle.class, hook);
+    findAndHookMethod("com.google.android.gms.auth.GoogleAuthUtil", lpparam.classLoader, "getToken", Context.class, Account.class, String.class, hook);
+    findAndHookMethod("com.google.android.gms.auth.GoogleAuthUtil", lpparam.classLoader, "getToken", Context.class, String.class, String.class, Bundle.class, hook);
+    findAndHookMethod("com.google.android.gms.auth.GoogleAuthUtil", lpparam.classLoader, "getToken", Context.class, String.class, String.class, hook);
+    //getTokenWithNotification 好像没有什么 app 用，就不写hook了
+
+  }
+
+  class GoogleAuthUtilGetTokenHook extends XC_MethodHook {
+    @Override
+    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+      for(int i = 0; i < param.args.length; i++) {
+        if( param.args[i] instanceof String ) {
+          String scope = (String) param.args[i];
+          if( scope.matches("(?i)^\\s*oauth2:.*") ){
+            if( scope.matches( ".*(\\s|^)https://www\\.googleapis\\.com/auth/drive(\\s|$).*") ) {
+              param.args[i] = scope.replace("https://www.googleapis.com/auth/drive", "https://www.googleapis.com/auth/drive.readonly");
+            }
+            break;
+          }
         }
       }
-    });
+    }
   }
 }
