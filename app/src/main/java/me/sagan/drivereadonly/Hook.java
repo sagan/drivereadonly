0557baf776cf654e91ea1020e4bd660195e326bb
Jacques De SAGAN <jacquesde@sagan.me>
2019-02-03 16:25:12 +0800

    improve

--- app/src/main/java/me/sagan/drivereadonly/Hook.java
+++ app/src/main/java/me/sagan/drivereadonly/Hook.java
@@ -20,45 +20,26 @@ public class Hook implements IXposedHookLoadPackage {
   public static final String DRIVE_READONLY = "https://www.googleapis.com/auth/drive.readonly";
 
   public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
-//  if (!lpparam.packageName.equals("com.android.systemui"))
-//     return;
-    XposedBridge.log("fuckyou Load package " + lpparam.packageName);
+//  if (!lpparam.packageName.equals("com.doubleTwist.cloudPlayer"))
+//    return;
+//  XposedBridge.log("fuckyou Load package " + lpparam.packageName);
 
     XSharedPreferences pref = new XSharedPreferences(Hook.class.getPackage().getName(), "config");
     pref.reload();
-
     if( !pref.getBoolean("enabled", false) ) {
       return;
     }
 
-    try {
-      findAndHookConstructor("com.google.android.gms.common.api.Scope", lpparam.classLoader, String.class, new XC_MethodHook() {
-        @Override
-        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-          String scope = (String) param.args[0];
-          XposedBridge.log("fuckyou hook scope " + scope);
-          if (scope.equals(DRIVE)) {
-            param.args[0] = DRIVE_READONLY;
-          }
-        }
-      });
-    } catch(Exception e) {
-
-    }
-
     XC_MethodHook scopeHook = new ScopeHook();
-    // 很多 app 使用 Collections.singletonList(DriveScopes.DRIVE) 这种方式创建 Scope. 所以这样简单粗暴地 hook - -
+    hookConstructor("com.google.android.gms.common.api.Scope", lpparam.classLoader, String.class, scopeHook);
+    // GMS 里创建 Scope 方法太多. 而且很多App代码都混淆了, 所以这样简单粗暴地 hook - -
     findAndHookMethod(Collections.class, "singletonList", Object.class, scopeHook);
     findAndHookMethod(Collections.class, "singleton", Object.class, scopeHook);
-    // findAndHookMethod(Arrays.class, "asList", Object[].class, scopeHook);
-
-
-
-    XC_MethodHook authoUtilHook = new GoogleAuthUtilGetTokenHook();
-    hookMethod("com.google.android.gms.auth.GoogleAuthUtil", lpparam.classLoader, "getToken", Context.class, Account.class, String.class, Bundle.class, authoUtilHook);
-    hookMethod("com.google.android.gms.auth.GoogleAuthUtil", lpparam.classLoader, "getToken", Context.class, Account.class, String.class, authoUtilHook);
-    hookMethod("com.google.android.gms.auth.GoogleAuthUtil", lpparam.classLoader, "getToken", Context.class, String.class, String.class, Bundle.class, authoUtilHook);
-    hookMethod("com.google.android.gms.auth.GoogleAuthUtil", lpparam.classLoader, "getToken", Context.class, String.class, String.class, authoUtilHook);
+    findAndHookMethod(android.os.Parcel.class, "writeString", String.class, scopeHook);
+    hookMethod("com.google.android.gms.auth.GoogleAuthUtil", lpparam.classLoader, "getToken", Context.class, Account.class, String.class, Bundle.class, scopeHook);
+    hookMethod("com.google.android.gms.auth.GoogleAuthUtil", lpparam.classLoader, "getToken", Context.class, Account.class, String.class, scopeHook);
+    hookMethod("com.google.android.gms.auth.GoogleAuthUtil", lpparam.classLoader, "getToken", Context.class, String.class, String.class, Bundle.class, scopeHook);
+    hookMethod("com.google.android.gms.auth.GoogleAuthUtil", lpparam.classLoader, "getToken", Context.class, String.class, String.class, scopeHook);
     //getTokenWithNotification 好像没有什么 app 用，就不写hook了
 
   }
@@ -69,22 +50,10 @@ public class Hook implements IXposedHookLoadPackage {
     } catch(Exception e) {}
   }
 
-  class GoogleAuthUtilGetTokenHook extends XC_MethodHook {
-    @Override
-    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-      for(int i = 0; i < param.args.length; i++) {
-        if( param.args[i] instanceof String ) {
-          String scope = (String) param.args[i];
-          if( scope.matches("(?i)^\\s*oauth2:.*") ){
-            XposedBridge.log("fuckyou hook AuthUtils " + scope);
-            if( scope.matches( ".*(\\s|:)https://www\\.googleapis\\.com/auth/drive(\\s|$).*") ) {
-              param.args[i] = scope.replace(DRIVE, DRIVE_READONLY);
-            }
-            break;
-          }
-        }
-      }
-    }
+  static void hookConstructor(String c, ClassLoader classLoader,  Object... parameterTypesAndCallback) {
+    try {
+      findAndHookConstructor(c, classLoader, parameterTypesAndCallback);
+    } catch(Exception e) {}
   }
 
   class ScopeHook extends XC_MethodHook {
@@ -93,8 +62,16 @@ public class Hook implements IXposedHookLoadPackage {
       for(int i = 0; i < param.args.length; i++) {
         if( param.args[i] instanceof String ) {
           String scope = (String) param.args[i];
-          if( scope.equals(DRIVE) ) {
+          if( scope.length() > 400 ) {
+            continue;
+          } else if( scope.equals(DRIVE) ) {
             param.args[i] = DRIVE_READONLY;
+            break;
+          } else if( scope.toLowerCase().startsWith("oauth2:") ){
+//            XposedBridge.log("fuckyou hook oauth2 " + scope);
+            if( scope.matches( ".*(\\s|:)https://www\\.googleapis\\.com/auth/drive(\\s|$).*") ) {
+              param.args[i] = scope.replace(DRIVE, DRIVE_READONLY);
+            }
             break;
           }
         }
